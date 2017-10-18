6fccd33641dd6e38560108e86f2648e5438ecb91
cgr <wurdalak@gmx.com>
2017-10-18 16:23:28 +0200

    Changes: - bootstraped Codeception - fixed namespace bug on Link (luckily it was used only in its local namespace, therefore no breakings changes) - unit test for Link

---
+++ tests/unit/LinkTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Test\ObjectivePHP\Gateway\MySql;
+
+use Codeception\Test\Unit;
+use ObjectivePHP\Gateway\MySql\Link;
+
+class LinkTest extends Unit
+{
+    public function createMysqlMock()
+    {
+        return $this->getMockBuilder(\mysqli::class)->disableOriginalConstructor()->getMock();;
+    }
+    /**
+     * @param callable[] ...$filters
+     * @dataProvider filtersProvider
+     */
+    public function testConstruct(callable ...$filters)
+    {
+        $mysqli = $this->createMysqlMock();
+
+        $link = new Link($mysqli, ...$filters);
+        $this->assertSame($mysqli, $link->getLink());
+        $this->assertSame($filters, $link->getFilters());
+    }
+
+    /**
+     * @param callable[] ...$filters
+     * @dataProvider filtersProvider
+     */
+    public function testGettersSetters(callable ...$filters)
+    {
+        $link = new Link($this->createMysqlMock());
+
+        $anotherMysqli = $this->createMysqlMock();
+        $this->assertSame($anotherMysqli, $link->setLink($anotherMysqli)->getLink());
+        $this->assertEquals([], $link->getFilters());
+
+        $link->setFilters($filters);
+        $this->assertSame($filters, $link->getFilters());
+
+    }
+
+    /**
+     * @param callable[] $filters
+     * @param $expectedValue
+     * @dataProvider filtersRunResultsProvider
+     */
+    public function testRunFilters($filters, $expectedValue)
+    {
+        $this->assertEquals($expectedValue, (new Link($this->createMysqlMock(), ...$filters))->runFilters());
+    }
+
+    public function filtersRunResultsProvider()
+    {
+        $filters = $this->filtersProvider();
+        $expected = [
+            'noFilters'       => [$filters['noFilters'], true],
+            'trueFilter'      => [$filters['trueFilter'], true],
+            'falseFilter'     => [$filters['falseFilter'], false],
+            'trueFalseFilter' => [$filters['trueFalseFilter'], false],
+            'falseTrueFilter' => [$filters['falseTrueFilter'], false],
+        ];
+        return $expected;
+    }
+
+    /**
+     * @return array
+     */
+    public function filtersProvider() : array
+    {
+        $trueFilter = function() {
+            return true;
+        };
+        $falseFilter = function() {
+            return false;
+        };
+        return [
+            'noFilters' => [
+            ],
+            'trueFilter' => [
+                $trueFilter,
+            ],
+            'falseFilter' => [
+                $falseFilter,
+            ],
+            'trueFalseFilter' => [
+                $trueFilter,
+                $falseFilter,
+            ],
+            'falseTrueFilter' => [
+                $falseFilter,
+                $trueFilter,
+            ],
+        ];
+    }
+}
